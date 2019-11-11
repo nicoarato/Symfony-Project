42bb547f93416185d1e6df88c1de823d6f9376e1
Nico Arato <nico.arato@gmail.com>
2019-11-11 16:42:02 -0300

    Guardando el usuario registrado.

--- proyecto-symfony/src/Controller/UserController.php
+++ proyecto-symfony/src/Controller/UserController.php
@@ -9,16 +9,39 @@ USe Symfony\Component\HttpFoundation\Request;
 
 use App\Entity\User;
 use App\Form\RegisterType;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends AbstractController
 {
     /**
      * @Route("/user", name="user")
      */
-    public function register(Request $request)
+    public function register(Request $request, UserPasswordEncoderInterface $encoder )
     {
+        //creo formulario
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
+        
+        //relleno objeto con datos del form
+        $form->handleRequest($request);
+        
+        //compruebo si envia
+        if($form->isSubmitted()){
+            //modifico objeto
+            $user->setRole('ROLE_USER');
+        
+            $user->setCreatedAt(new \DateTime('now'));
+            //cifrar pass
+            $encoded = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($encoded);
+            
+          //guardar usuario
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+            
+            return $this->redirectToRoute('tasks');
+        }
         
         return $this->render('user/register.html.twig', [
             'form' => $form->createView()
--- proyecto-symfony/src/Entity/User.php
+++ proyecto-symfony/src/Entity/User.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * User
@@ -11,7 +12,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="users")
  * @ORM\Entity
  */
-class User
+class User implements UserInterface
 {
     /**
      * @var int
@@ -140,12 +141,12 @@ class User
         return $this;
     }
 
-    public function getCreatedAt(): ?\DateTimeInterface
+    public function getCreatedAt()
     {
         return $this->createdAt;
     }
 
-    public function setCreatedAt(?\DateTimeInterface $createdAt): self
+    public function setCreatedAt($createdAt): self
     {
         $this->createdAt = $createdAt;
 
@@ -159,5 +160,23 @@ class User
     public function getTasks(): Collection{
         
         return $this->tasks;
+    }
+    
+    //metodos del UserInterface
+    
+    public function getUsername(){
+        return $this->email;
+    }
+    
+    public function getSalt(){
+        return null;
+    }
+    
+    public function getRoles(){
+        return array('ROLE_USER');
+    }
+    
+    public function eraseCredentials(){
+        
     }
 }
